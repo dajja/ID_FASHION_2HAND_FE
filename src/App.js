eb65e69d10bee619f71cccf8091dd45941be6f8b
manhjjjjj <pubbg21@gmail.com>
2023-03-23 23:34:55 +0700

    feature/#1037/look_book

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import './App.scss';
+import LookBookPage from './LookBook/LookBookPage';
 // import ForgotPasswordPage from './components/ForgotPassword/ForgotPasswordPage'
 // import SignUpPage from './components/SignUp/SignUpPage';
 // import SignInPage from './components/SingIn/SignInPage';
-import ShoppingCartPage from './Shopping_cart/ShoppingCartPage';
+// import ShoppingCartPage from './Shopping_cart/ShoppingCartPage';
 
 function App() {
   return (
     // <ForgotPasswordPage/>
     // <SignUpPage/>
     // <SignInPage/>
-    <ShoppingCartPage/>
+    // <ShoppingCartPage/>
+    <LookBookPage/>
   );
 }
 
