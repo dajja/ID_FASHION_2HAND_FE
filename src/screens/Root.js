2cacdf36ee2fb3887a62d40a652318c70d3e374b
daijja <buiminh8690@gmail.com>
2023-03-23 15:47:26 +0700

    feature/#1038/my_account

--- src/screens/Root.js
+++ src/screens/Root.js
@@ -4,6 +4,7 @@ import React from "react";
 import SignInPage from "../components/SingIn/SignInPage";
 import SignUpPage from '../components/SignUp/SignUpPage';
 import Homepage from "../pages/homepage/Homepage";
+import Account from "../pages/accountPage/Account";
 function Root() {
     return (
         <>
@@ -12,6 +13,7 @@ function Root() {
                     <Route path="/signup" element={<SignUpPage/>} />
                     <Route path="/signin" element={<SignInPage/>} />
                     <Route path="/" element={<Homepage/>} />
+                    <Route path="/username" element={<Account/>} />
                 </Routes>
             </BrowserRouter>
         </>
